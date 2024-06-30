3ef7f0d3818ad9bfae0dae2b3e80d2eb3c74ebab
Priyanshu Verma <priyanshuverma@outlook.in>
2024-06-30 11:15:23 +0530

    models export file

---
+++ lib/models.dart
@@ -0,0 +1,28 @@
+export './common/models/download_url_model.dart';
+
+export './modules/album/models/album_model.dart';
+
+export './modules/artists/models/artist_albums_model.dart';
+export './modules/artists/models/artist_map_model.dart';
+export './modules/artists/models/artist_model.dart';
+export './modules/artists/models/artist_songs_model.dart';
+
+export './modules/explore/models/browse_channel_model.dart';
+export './modules/explore/models/browse_chart_model.dart';
+export './modules/explore/models/browse_model.dart';
+export './modules/explore/models/browse_radio_model.dart';
+export './modules/explore/models/browse_trending_model.dart';
+
+export './modules/playlist/models/playlist_map_model.dart';
+export './modules/playlist/models/playlist_model.dart';
+
+export './modules/search/models/search_album_model.dart';
+export './modules/search/models/search_artist_model.dart';
+export './modules/search/models/search_playlist_model.dart';
+export './modules/search/models/search_result_model.dart';
+export './modules/search/models/search_song_model.dart';
+export './modules/search/models/search_topquery_model.dart';
+
+export './modules/song/models/song_lyrics_model.dart';
+export './modules/song/models/song_model.dart';
+export './modules/song/models/song_radio_model.dart';
